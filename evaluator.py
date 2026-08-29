8ffa2b7e093a21bef3b911762f8ec0719bd5e058
Riwaj Shrestha <riwazshrestha20@gmail.com>
2026-08-29 13:25:01 +0930

    Setting up project files

---
+++ evaluator.py
@@ -0,0 +1,14 @@
+"""
+HIT137 Group Assesment 2
+RIWAJ SHRESTHA
+SAWAN GURUNG
+JUNG-CHUAN CHIANG
+
+Question 2 - Expression evaluator using recursive descent parsing.
+
+Reads one expression per line from a text file and writes a four line block
+(Input, Tree, Tokens, Result) for each one into output.txt .
+"""
+
+DIGITS = "0123456789"
+OPERATORS = "+-*/%^"
